6c618781ee3f61bfb7c0febea8e6105a9e0dfaf4
Julio <jcjauer@DESKTOP-6OP08OC.localdomain>
2022-09-15 16:20:11 -0400

    Feat: GeneralizedLinkedList

--- GeneralizedLinkedList.c
+++ GeneralizedLinkedList.c
@@ -19,16 +19,20 @@ int addAtom(Node **list, int atom) {
 
     log_debug("newNode->(type, atomList, next): (%d, %p, %p)", newNode->type, newNode->atomList.atom, newNode->next);
 
-    Node *aux = *list;
-    while(aux->next!=NULL) {
-        aux = aux->next;
+    if(*list == NULL) {
+        *list = newNode;
     }
-    aux->next = newNode;
+    else {
+        Node *aux = NULL;
+        for(aux = *list; aux->next != NULL; aux = aux->next);
+        aux->next = newNode;
+    }
 
     log_trace("addAtom <-");
+    return 1;
 }
 int addList(Node **list, Node **subList) {
-    log_info("Adicionando Lista");
+    log_info("Adicionando lista");
     log_trace("addList ->");
     Node *newNode = (Node *)malloc(sizeof(Node));
 
@@ -43,26 +47,117 @@ int addList(Node **list, Node **subList) {
 
     log_debug("newNode->(type, atomList, next): (%d, %p, %p)", newNode->type, newNode->atomList.list, newNode->next);
 
-    Node *aux = *list;
-    while(aux->next!=NULL) {
-        aux = aux->next;
+    if(*list == NULL) {
+        *list = newNode;
     }
-    aux->next = newNode;
+    else {
+        Node *aux = NULL;
+        for(aux = *list; aux->next != NULL; aux = aux->next);
+        aux->next = newNode;
+    }
 
     log_trace("addList <-");
+    return 1;
 }
 Node* head(Node *list) {
-
+    log_info("Retornado a cabeça da lista");
+    log_trace("head ->");
+    
+    if(list == NULL) {
+        log_trace("head <-");
+        return list;
+    }
+    else {
+        Node *headAux = NULL;
+        if(list->type == 0) {
+            headAux = (Node *)malloc(sizeof(Node));
+            if(headAux == NULL) {   
+                log_trace("head <-");
+                return headAux;
+            }
+            headAux->type = 1;
+            headAux->atomList.list = list->atomList.list;
+            headAux->next = NULL;
+        }
+        else {
+            headAux = list->atomList.list;    
+        }
+        log_trace("head <-");
+        return headAux;
+    }   
 }
 Node* tail(Node *list) {
+    log_info("Retornado a cauda da lista");
+    log_trace("tail ->");
 
+    if(list == NULL) {
+        log_trace("tail <-");
+        return list;
+    }
+    else {
+        log_trace("tail <-");
+        return list->next;
+    }   
 }
 void show(Node *list) {
-
+    log_info("Mostrando toda a lista");
+    log_trace("show ->");
+    printf("( ");
+    Node *aux = NULL;
+    for(aux = list; aux != NULL; aux = aux->next) {
+        if (aux->type == 0) {
+            printf("%d ",aux->atomList.atom);
+        } else {
+            show(aux->atomList.list);
+        }
+    }
+    printf(")");
+    log_trace("show <-");
 }
 bool search(Node *list, int atom) {
-
+    log_info("Buscando existência de átomo na lista");
+    log_trace("search ->");
+    bool currentBool = false;
+    if(list != NULL) {
+        Node *aux = NULL;
+        for(aux = list; aux != NULL; aux = aux->next) {
+            if (aux->type == 1) {
+                bool newBool = search(aux->atomList.list, atom);
+                if(newBool == true) {
+                    currentBool = true;
+                    log_trace("search <-");
+                    return currentBool;
+                }
+            }
+            else {
+                if(aux->atomList.atom == atom) {
+                    currentBool = true;
+                    log_trace("search <-");
+                    return currentBool;
+                }
+            }
+        }
+    }
+    log_trace("search <-");
+    return currentBool;
 }
 int depth(Node *list) {
-
+    log_info("Buscando profindidade da lista");
+    log_trace("depth ->");
+    int currentDepth = 0;
+    if(list == NULL) {
+        log_trace("depth <-");
+        return 0;
+    }
+    Node *aux = NULL;
+    for(aux = list; aux != NULL; aux = aux->next) {
+        if(aux->type == 1) {
+            int newDepth = depth(aux->atomList.list);
+            if(newDepth > currentDepth) {
+            currentDepth = newDepth;
+            }
+        }
+    }
+    log_trace("depth <-");
+    return currentDepth + 1;
 }
--- GeneralizedLinkedListTest.c
+++ GeneralizedLinkedListTest.c
@@ -1,10 +1,53 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "GeneralizedLinkedList.h"
-#include "log.h"
 
-int main()  {
-    log_set_level(LOG_TRACE);
- 
-    return EXIT_SUCCESS;
+int main() {
+    Node *listaGeneralizada = NULL;
+    printf("Lista: ");
+    show(listaGeneralizada);
+    addAtom(&listaGeneralizada, 3);
+    addAtom(&listaGeneralizada, 5);
+    //printf("%d\n",listaGeneralizada->atomList.atomo);
+    Node *lista2 = NULL;
+    addAtom(&lista2, 7);
+    addAtom(&lista2, 9);
+    Node *lista3 = NULL;
+    addAtom(&lista3, 11);
+    addAtom(&lista3, 13);
+
+    Node *lista4 = NULL;
+    addAtom(&lista4, 15);
+    addAtom(&lista4, 17);
+    addList(&lista2, &lista4);
+    
+    Node *lista5 = NULL;
+    addAtom(&lista5, 33);
+    addAtom(&lista5, 44);
+    addList(&lista3, &lista5);
+    
+    //    printf("%d\n",lista2->prox->atomList.atomo);
+    addList(&listaGeneralizada, &lista2);
+    addList(&listaGeneralizada, &lista3);
+
+    //    printf("%d\n",listaGeneralizada->prox->prox->atomList.lista->atomList.atomo);
+    
+    printf("\nLista: ");
+    show(listaGeneralizada);
+    printf("\nProfundidade: %d\n",depth(listaGeneralizada));
+    
+    printf("\nCauda: ");
+    show(tail(listaGeneralizada));
+    
+    printf("\n\nCabeca da Cauda: %d\n",head(tail(listaGeneralizada))->atomList.atom);
+    printf("\n\nCabeca(Cauda(Cauda(Cabeca(Cauda(Cauda))))):\n");
+    
+    show(head(tail(tail(head(tail(tail(listaGeneralizada)))))));
+    
+    printf("\n\nLista: ");
+    show(head(tail(tail(listaGeneralizada))));
+
+    printf("\nProfundidade: %d\n",depth(head(tail(tail(listaGeneralizada)))));
+    
+    return 0;
 }
